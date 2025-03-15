1ce3f012a69400edc434b7397116cc7dd84319bb
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-15 21:54:36 +0500

    Outfit Font is Added 19:21

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h2>Mein Hun Jian Aur Mere Pyare Papa</h2>
+      <h2>Mein Hun Jian Aur Mein Hun Bohot Taqatwar Mein Hun Bohot Surila</h2>
       <Button>This is called Button</Button>
     </div>
   );
