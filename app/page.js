1f2e2c4ebf00149dea2f2d71839d8497828f1011
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-15 21:47:12 +0500

    Just installed everything

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Mein Hun Jian Aur Mere Pyare Papa</h2>
+      <Button>This is called Button</Button>
+    </div>
+  );
+}
