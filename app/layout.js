bb247850bd5c2e9db3b8c71bc824e19559f5e419
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-15 22:03:12 +0500

    Theme Provider Added 23:51

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,20 @@
-import { Outfit  } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
-
+import Provider from "./provider";
 
 export const metadata = {
   title: "AI YOUTUBE SHORT VIDEO GENERATOR",
-  description: "This SASS Product Is Being Developed By Muhammad Hassaan Mughal",
+  description:
+    "This SASS Product Is Being Developed By Muhammad Hassaan Mughal",
 };
 
-const outfit=Outfit({subsets:['latin']})
+const outfit = Outfit({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-      className={outfit.className}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={outfit.className}>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
