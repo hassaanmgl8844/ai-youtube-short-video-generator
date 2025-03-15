970c79400e026f9445c85aa08c230317616e7d72
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-15 22:20:09 +0500

    Started Working on Hero Section , Header is now Completed | 30:00

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Mein Hun Jian Aur Mein Hun Bohot Taqatwar Mein Hun Bohot Surila</h2>
-      <Button>This is called Button</Button>
+    <div className="md:px-16 lg:px-24 xl:px-36">
+      {/* Header */}
+      <Header />
+      {/* Hero  */}
+      <Hero />
     </div>
   );
 }
