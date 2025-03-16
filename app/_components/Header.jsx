10eccd03757d1ec1b1c397f37aeafa481baf5439
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-16 17:42:26 +0500

    User Logo Showed in the Header | 51:37

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,9 +1,13 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 import Authentication from "./Authentication";
+import { useAuthContext } from "../provider";
+import Link from "next/link";
 
 const Header = () => {
+  const { user } = useAuthContext();
   return (
     <div className="p-4 flex items-center justify-between">
       <div className="flex items-center gap-3">
@@ -11,9 +15,18 @@ const Header = () => {
         <h2 className="text-2xl font-bold">YT Video Gen</h2>
       </div>
       <div>
-        <Authentication>
-          <Button>Get Started</Button>
-        </Authentication>
+        {!user ? (
+          <Authentication>
+            <Button>Get Started</Button>
+          </Authentication>
+        ) : (
+          <div className="flex items-center gap-3">
+            <Link href={'/dashboard'}>
+            <Button>Dashboard</Button>
+            </Link>
+            <Image src={user?.photoURL} alt="userImage" width={40} height={40} className="rounded-full" />
+          </div>
+        )}
       </div>
     </div>
   );
