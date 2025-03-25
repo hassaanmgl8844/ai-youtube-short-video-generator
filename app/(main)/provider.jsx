f05eef80c48c6a1d2be7398758c3a39254460b20
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-25 17:52:15 +0500

    SideBar Collapser Added | 01:28:03

--- app/(main)/provider.jsx
+++ app/(main)/provider.jsx
@@ -1,14 +1,18 @@
-import { SidebarProvider } from '@/components/ui/sidebar'
-import React from 'react'
-import AppSiderbar from './_components/AppSiderbar'
+import { SidebarProvider } from "@/components/ui/sidebar";
+import React from "react";
+import AppSiderbar from "./_components/AppSiderbar";
+import AppHeader from "./_components/AppHeader";
 
-const DashboardProvider = ({children}) => {
+const DashboardProvider = ({ children }) => {
   return (
     <SidebarProvider>
-        <AppSiderbar />
-    <div>{children}</div>
+      <AppSiderbar />
+      <div className="w-full">
+        <AppHeader />
+        {children}
+      </div>
     </SidebarProvider>
-  )
-}
+  );
+};
 
-export default DashboardProvider
+export default DashboardProvider;
