33ce15435126e7fea53bfceaec3059f323bb58ff
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-25 17:54:48 +0500

    Added a Logo in the SideBar | 01:28:42

--- app/(main)/_components/AppSiderbar.jsx
+++ app/(main)/_components/AppSiderbar.jsx
@@ -6,11 +6,13 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import Image from "next/image";
 
 const AppSiderbar = () => {
   return (
     <Sidebar>
       <SidebarHeader />
+      <Image src={'/logo.svg'} alt="logo" width={40} height={40} />
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
