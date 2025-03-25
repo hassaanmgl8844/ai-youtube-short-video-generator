f740efd260eca36e859c1fb16f71cb11b2c669ef
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-03-25 18:00:34 +0500

    Side Bar Header Is Ready | 01:30:49

--- app/(main)/_components/AppSiderbar.jsx
+++ app/(main)/_components/AppSiderbar.jsx
@@ -12,7 +12,13 @@ const AppSiderbar = () => {
   return (
     <Sidebar>
       <SidebarHeader />
-      <Image src={'/logo.svg'} alt="logo" width={40} height={40} />
+      <div className="">
+        <div className="flex items-center gap-3 w-full justify-center mt-5">
+          <Image src={"/logo.svg"} alt="logo" width={40} height={40} />
+          <h2 className="font-bold text-2xl">Video Gen</h2>
+        </div>
+        <h2 className="text-lg text-gray-400 text-center mt-3">Ai Short Video Generator</h2>
+      </div>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
